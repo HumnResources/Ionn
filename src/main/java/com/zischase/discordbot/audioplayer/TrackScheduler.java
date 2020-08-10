a08ddf22d45bedab995cd75c9c486e3160281fda
Chase <ziskachase@gmaill.com>
2020-08-10 13:33:31 -0400

    -- Added repeat functionality -- -- Currently still reworking exception catching/errors. --

--- src/main/java/com/zischase/discordbot/audioplayer/TrackScheduler.java
+++ src/main/java/com/zischase/discordbot/audioplayer/TrackScheduler.java
@@ -20,41 +20,40 @@ public class TrackScheduler extends AudioEventAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(TrackScheduler.class);
     private final AudioPlayer player;
     private final BlockingQueue<AudioTrack> queue;
-    private final AudioManager manager;
 
+    private boolean repeat = false;
     private AudioTrack lastTrack;
     private TextChannel textChannel = null;
 
     public TrackScheduler(AudioManager manager) {
         this.player = manager.getPlayer();
-        this.manager = manager;
         this.queue = new LinkedBlockingQueue<>();
 
         LOGGER.info("TrackScheduler created.");
     }
 
-    public void queue(AudioTrack track, TextChannel textChannel) {
-        this.textChannel = textChannel;
+    public void setRepeat(boolean repeat) {
+        this.repeat = repeat;
+    }
 
-        if (manager.getPlayer().getPlayingTrack() != null)
-            textChannel.sendMessage("Added `" + track.getInfo().title + "` to the queue.").queue();
+    public boolean isRepeat() {
+        return repeat;
+    }
+
+    public void queueAudio(AudioTrack track, TextChannel textChannel) {
+        this.textChannel = textChannel;
+        boolean hasPlayingTrack = GuildManager.getContext(textChannel.getGuild())
+                .getAudioManager()
+                .getPlayer()
+                .getPlayingTrack() != null;
+
+        if (hasPlayingTrack)
+            textChannel.sendMessage("Added `" + track.getInfo().title + "` to the queue.")
+                    .queue();
 
         if (!player.startTrack(track, true)) { // noInterrupt: True == add to queue
             queue.offer(track);
         }
-    }
-
-    public ArrayList<AudioTrack> getQueue() {
-        ArrayList<AudioTrack> trackList = new ArrayList<>();
-
-        if (!this.queue.isEmpty())
-            trackList.addAll(queue);
-
-        return trackList;
-    }
-
-    public void clearQueue() {
-        this.queue.clear();
     }
 
     public void queueList(ArrayList<AudioTrack> tracks, TextChannel channel) {
@@ -69,16 +68,32 @@ public class TrackScheduler extends AudioEventAdapter {
 
         if (player.isPaused())
             player.setPaused(false);
+
         else if (player.getPlayingTrack() == null)
             player.startTrack(queue.poll(), false);
     }
 
+    public void clearQueue() {
+        this.queue.clear();
+    }
+
+    public ArrayList<AudioTrack> getQueue() {
+        ArrayList<AudioTrack> trackList = new ArrayList<>();
+
+        if (!this.queue.isEmpty())
+            trackList.addAll(queue);
+
+        return trackList;
+    }
+
     public void nextTrack() {
-        this.player.startTrack(queue.poll(), false);
+        this.player
+                .startTrack(queue.poll(), false);
     }
 
     public void prevTrack() {
-        this.player.startTrack(lastTrack, false);
+        this.player
+                .startTrack(lastTrack, false);
     }
 
     @Override
@@ -91,21 +106,28 @@ public class TrackScheduler extends AudioEventAdapter {
     @Override
     public void onPlayerResume(AudioPlayer player) {
         GuildManager.getContext(textChannel.getGuild())
-            .getPlayerPrinter()
-            .printNowPlaying(textChannel);
+                .getPlayerPrinter()
+                .printNowPlaying(textChannel);
     }
 
     @Override
     public void onTrackStart(AudioPlayer player, AudioTrack track) {
         GuildManager.getContext(textChannel.getGuild())
                 .getPlayerPrinter()
-                .printNowPlaying(textChannel);
+                .printQueue(textChannel);
     }
 
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
         if (endReason.equals(AudioTrackEndReason.LOAD_FAILED)) {
-            if (lastTrack.getInfo().identifier.equalsIgnoreCase(track.getInfo().identifier)) {
+            boolean tooManyAttempts = false;
+
+            if (lastTrack != null)
+                tooManyAttempts = lastTrack.getInfo()
+                        .identifier
+                        .equalsIgnoreCase(track.getInfo().identifier);
+
+            if (tooManyAttempts) {
                 textChannel.sendMessage("Sorry, too many errors. \nGotta move on.")
                         .complete()
                         .delete()
@@ -129,15 +151,35 @@ public class TrackScheduler extends AudioEventAdapter {
         }
 
         this.lastTrack = track.makeClone();
+
+        if (repeat) {
+            queue.add(track.makeClone());
+        }
     }
 
     @Override
     public void onTrackException(AudioPlayer player, AudioTrack track, FriendlyException exception) {
+        if (lastTrack.getIdentifier().matches(track.getIdentifier())) {
+            textChannel
+                    .sendMessage("There was an error playing the song, I tried twice but... \nI'm afraid we have to move on.")
+                    .complete()
+                    .delete()
+                    .queueAfter(2000, TimeUnit.MILLISECONDS);
+        }
+        else {
+            player
+                    .playTrack(track.makeClone());
+        }
+        lastTrack = track;
     }
 
     @Override
     public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs) {
-        manager.getPlayer().stopTrack();
+        GuildManager.getContext(textChannel.getGuild())
+                .getAudioManager()
+                .getPlayer()
+                .stopTrack();
+
         if (!queue.isEmpty()) {
             player.playTrack(queue.poll());
         }
