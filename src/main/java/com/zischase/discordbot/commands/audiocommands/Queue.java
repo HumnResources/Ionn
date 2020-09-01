380b83047cc9dee2c7002ddfe577545624857709
Chase <ziskachase@gmaill.com>
2020-08-31 20:50:46 -0400

    -- Refactored code into handle method, reuse more code this way. --
    -- Added feature to remove single item from queue --

--- src/main/java/com/zischase/discordbot/commands/audiocommands/Queue.java
+++ src/main/java/com/zischase/discordbot/commands/audiocommands/Queue.java
@@ -6,7 +6,6 @@ import com.zischase.discordbot.commands.Command;
 import com.zischase.discordbot.commands.CommandContext;
 import com.zischase.discordbot.guildcontrol.GuildManager;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -43,79 +42,69 @@ public class Queue extends Command
 		
 		if (! args.isEmpty())
 		{
-			if (args.get(0)
-					.matches("(?i)(-clear|-c)"))
+			if (args.size() == 1)
 			{
-				EmbedBuilder embed = new EmbedBuilder();
-				embed.setColor(Color.BLUE);
-				scheduler.clearQueue();
-				
-				embed.appendDescription("Queue cleared.");
-				ctx.getChannel()
-				   .sendMessage(embed.build())
-				   .queue();
-			}
-			else if (args.get(0)
-						 .matches("(?i)(\\d+)"))
-			{
-				bringToFront(Integer.parseInt(args.get(0)), scheduler, ctx.getChannel());
-				
-			}
-			else if (args.get(0)
-						 .matches("(?i)(-jump|-j)"))
-			{
-				if (args.get(1)
-						.matches("(?i)(\\d+)"))
+				if (args.get(0)
+						.matches("(?i)-(clear|c)"))
 				{
-					jumpTo(Integer.parseInt(args.get(1)), scheduler, ctx.getChannel());
+					EmbedBuilder embed = new EmbedBuilder();
+					embed.setColor(Color.BLUE);
+					scheduler.clearQueue();
+					
+					embed.appendDescription("Queue cleared.");
+					ctx.getChannel()
+					   .sendMessage(embed.build())
+					   .queue();
+				}
+			}
+			else if (args.size() == 2 && args.get(1)
+										.matches("(?i)(\\d+)"))
+			{
+				ArrayList<AudioTrack> queue = scheduler.getQueue();
+				int index = Integer.parseInt(args.get(1));
+				
+				if (index < 2 || index > queue.size())
+				{
+					return;
+				}
+				index = index - 1; // Subtract 1 for '0' based numeration.
+				
+				if (args.get(0)
+						.matches("(?i)-(next|n)"))
+				{
+					queue.add(0, queue.get(index));
+					queue.remove(index + 1); // Adding one to account for -> shift of list
+					
+					scheduler.clearQueue();
+					scheduler.queueList(queue, ctx.getChannel());
+				}
+				else if (args.get(0)
+							 .matches("(?i)-(jump|jumpto|j)"))
+				{
+					queue.addAll(queue.subList(0, index - 1));
+					
+					ArrayList<AudioTrack> newQueue = new ArrayList<>(queue.subList(index, queue.size()));
+					
+					scheduler.clearQueue();
+					scheduler.queueList(newQueue, ctx.getChannel());
+				}
+				else if (args.get(0)
+							 .matches("(?i)-(clear|c)"))
+				{
+					queue.remove(index);
+					
+					scheduler.clearQueue();
+					scheduler.queueList(queue, ctx.getChannel());
 				}
 			}
 		}
-		
 		
 		GuildManager.getContext(ctx.getGuild())
 					.getPlayerPrinter()
 					.printQueue(ctx.getChannel());
 		
+		GuildManager.getContext(ctx.getGuild())
+					.getPlayerPrinter()
+					.printNowPlaying(ctx.getChannel());
 	}
-	
-	
-	private void jumpTo(int index, TrackScheduler scheduler, TextChannel textChannel)
-	{
-		if (index < 2 || index > scheduler.getQueue()
-										  .size())
-		{
-			return;
-		}
-		index = index - 1; // Subtract 1 for '0' based numeration.
-		
-		ArrayList<AudioTrack> queue = scheduler.getQueue();
-		
-		queue.addAll(queue.subList(0, index - 1));
-		
-		ArrayList<AudioTrack> newQueue = (ArrayList<AudioTrack>) queue.subList(index, queue.size());
-		
-		scheduler.clearQueue();
-		scheduler.queueList(newQueue, textChannel);
-	}
-	
-	private void bringToFront(int index, TrackScheduler scheduler, TextChannel textChannel)
-	{
-		if (index < 2 || index > scheduler.getQueue()
-										  .size())
-		{
-			return;
-		}
-		index = index - 1; // Subtract 1 for '0' based numeration.
-		
-		ArrayList<AudioTrack> queue = scheduler.getQueue();
-		
-		queue.add(0, queue.get(index));
-		queue.remove(index);
-		
-		scheduler.clearQueue();
-		scheduler.queueList(queue, textChannel);
-	}
-	
-	
 }
