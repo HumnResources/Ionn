6207b8b59436d4fc9f76eb6045b904a88c478ea4
HumnResourcs <ziskachase@gmail.com>
2020-12-02 22:54:50 -0500

    Added shutdown handler for SIGTERM heroku command

--- src/main/java/com/zischase/discordbot/Bot.java
+++ src/main/java/com/zischase/discordbot/Bot.java
@@ -1,14 +1,19 @@
 package com.zischase.discordbot;
 
+import com.zischase.discordbot.commands.CommandManager;
+import me.duncte123.botcommons.BotCommons;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 
 public class Bot
 {
 	private static JDA jda = null;
+	private static final Logger LOGGER = LoggerFactory.getLogger(Bot.class);
 	
 	static
 	{
@@ -31,6 +36,27 @@ public class Bot
 	
 	public static void main(String[] args) throws LoginException
 	{
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				LOGGER.warn("SHUTTING DOWN . . .");
+
+				jda.shutdownNow();
+				CommandManager.shutdown();
+				BotCommons.shutdown(jda);
+
+				LOGGER.info("Successful Shutdown");
+			}
+		});
+		//Operating system sends SIGFPE to the JVM
+		//the JVM catches it and constructs a
+		//ArithmeticException class, and since you
+		//don't catch this with a try/catch, dumps
+		//it to screen and terminates.  The shutdown
+		//hook is triggered, doing final cleanup.
+
+
 		jda.getPresence()
 		   .setActivity(Activity.watching("Everything"));
 	}
