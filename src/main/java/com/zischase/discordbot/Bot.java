e414b0971327fcc6ebd64191730b6c26fc3b901f
HumnResourcs <ziskachase@gmail.com>
2020-12-05 14:37:21 -0500

    -

--- src/main/java/com/zischase/discordbot/Bot.java
+++ src/main/java/com/zischase/discordbot/Bot.java
@@ -39,7 +39,7 @@ public class Bot
 	public static void main(String[] args)
 	{
 		jda.getPresence()
-				.setActivity(Activity.listening("For Music"));
+				.setActivity(Activity.listening(" music"));
 	}
 
 	private static void setShutdownHook()
