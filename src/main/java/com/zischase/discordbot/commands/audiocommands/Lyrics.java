938b2841108c36da9fa3cd386801b13f473bf0d6
HumnResourcs <ziskachase@gmail.com>
2020-12-07 22:06:04 -0500

    Testing useragents to work on heroku

--- src/main/java/com/zischase/discordbot/commands/audiocommands/Lyrics.java
+++ src/main/java/com/zischase/discordbot/commands/audiocommands/Lyrics.java
@@ -49,7 +49,7 @@ public class Lyrics extends Command
 		Document doc;
 		try {
 			 doc = Jsoup.connect(query)
-					.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+//					.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 					.get();
 		}
 		catch (IOException e)
