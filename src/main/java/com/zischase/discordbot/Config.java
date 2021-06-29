beb71254a9b04f3de82c364180254233453a49c5
HumnResourcs <ziskachase@gmail.com>
2021-06-29 13:35:53 -0400

    update dotenv dep. Fixed issue with loading env vars without .env file located

--- src/main/java/com/zischase/discordbot/Config.java
+++ src/main/java/com/zischase/discordbot/Config.java
@@ -4,13 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config
 {
-	
-	private static final Dotenv dotEnv = Dotenv.load();
-	
 	public static String get(String key)
 	{
 		if (System.getenv().isEmpty() || ! System.getenv().containsKey(key))
-			return dotEnv.get(key);
+			return Dotenv.load().get(key);
 
 		return System.getenv(key.toUpperCase());
 	}
