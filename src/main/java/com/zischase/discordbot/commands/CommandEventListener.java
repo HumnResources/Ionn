b90b1d92ff99c0d150600cc400ea77c6f4a036fc
HumnResourcs <ziskachase@gmail.com>
2021-07-03 22:42:15 -0400

    This should make it update any changes to the slash command API slug

--- src/main/java/com/zischase/discordbot/commands/CommandEventListener.java
+++ src/main/java/com/zischase/discordbot/commands/CommandEventListener.java
@@ -158,7 +158,7 @@ public class CommandEventListener extends ListenerAdapter {
 				/* Reinitialize the commands */
 				for (com.zischase.discordbot.commands.Command c : GuildContext.get(g.getId()).commandHandler().getCommandList()) {
 					/* Comparator to ensure we don't overwrite */
-					if (slashCommands.stream().noneMatch((sc) -> sc.getName().equalsIgnoreCase(c.getName())))
+					if (slashCommands.stream().noneMatch((sc) -> sc.toString().equals(c.toString())))
 						g.upsertCommand(c.getCommandData()).queue((cmd) -> LOGGER.info("Added slash command {} to server {} ", cmd.getName(), g.getName()));
 				}
 			}
