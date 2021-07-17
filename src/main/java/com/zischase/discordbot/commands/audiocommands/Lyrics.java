4702f73b45397afd229f01182441a8502ec2ea47
HumnResourcs <ziskachase@gmail.com>
2021-07-17 11:17:44 -0400

    Add variable proxy support. Needs testing

--- src/main/java/com/zischase/discordbot/commands/audiocommands/Lyrics.java
+++ src/main/java/com/zischase/discordbot/commands/audiocommands/Lyrics.java
@@ -1,5 +1,6 @@
 package com.zischase.discordbot.commands.audiocommands;
 
+import com.google.gson.Gson;
 import com.zischase.discordbot.commands.Command;
 import com.zischase.discordbot.commands.CommandContext;
 import com.zischase.discordbot.commands.CommandHandler;
@@ -21,46 +22,106 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.concurrent.CompletableFuture;
 
-/*
- * TODO : Find a solution for server rejection from hosting service
- */
+public final class Lyrics extends Command {
 
-public class Lyrics extends Command {
+	final class GeonodeProxyList {
+		int total;
+		int page;
+		int limit;
+		private final ArrayList<Data> data = new ArrayList<>();
 
-	private static final String PROXY_LIST_URL       = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt";
-	private static final String PROXY_LIST_FILE_NAME = "proxy-list.txt";
-	private static final Logger LOGGER               = LoggerFactory.getLogger(Lyrics.class);
-	private static final Timer  TIMER                = new Timer();
-	private static final int    MAX_MESSAGE_SIZE     = 2000;
-	private static final Map<String, Integer> proxies = new HashMap<>();
+		final class Data {
+			private String            id;
+			private String            ip;
+			private String            level;
+			private String            asn;
+			private String            city;
+			private String            country;
+			private String            createdAt;
+			private String            google;
+			private String            isp;
+			private String            lastChecked;
+			private String            latency;
+			private String            org;
+			private String            port;
+			private ArrayList<String> protocols;
+			private String            region;
+			private String            responseTime;
+			private String            speed;
+			private String            updatedAt;
+			private String            workingPercent;
+		}
+	}
+
+
+	private static final String LYRICS_PROVIDER_SEARCH_URI = "https://search.azlyrics.com/search.php?q=";
+	private static final String     PROXY_LIST_URL       = "https://proxylist.geonode.com/api/proxy-list?limit=50&page=1&sort_by=lastChecked&sort_type=desc&google=true";//"https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt";
+	private static final String     PROXY_LIST_FILE_NAME = "proxy-list.csv";
+	private static final String     USER_AGENT           = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
+	private static final Logger     LOGGER               = LoggerFactory.getLogger(Lyrics.class);
+	private static final Timer      TIMER                = new Timer();
+	private static final int        MAX_MESSAGE_SIZE     = 2000;
+	private static final int        TIMEOUT_MS           = 30000;
+	private static final List<GeonodeProxyList.Data> ACTIVE_PROXIES       = new ArrayList<>();
 
 	/* Initialize list of free proxy forwarders */
 	static {
+		StringBuilder csv = new StringBuilder();
 		try {
-			File proxyList = new File(PROXY_LIST_FILE_NAME);
-			if (!proxyList.exists() && !proxyList.isDirectory()) {
+			File proxyListFile = new File(PROXY_LIST_FILE_NAME);
+			if (proxyListFile.exists()) {
+				Files.delete(Path.of(proxyListFile.getAbsolutePath()));
+			}
+
+			if (!proxyListFile.exists() && !proxyListFile.isDirectory()) {
 				URL                 url          = new URL(PROXY_LIST_URL);
 				ReadableByteChannel byteChannel  = Channels.newChannel(url.openStream());
 				FileOutputStream    outputStream = new FileOutputStream(PROXY_LIST_FILE_NAME);
 				outputStream.getChannel().transferFrom(byteChannel, 0, Integer.MAX_VALUE);
+				LOGGER.info("Downloaded proxy list to file.");
 			}
 
-			Scanner  scanner = new Scanner(proxyList);
-			String[] tmp;
-			while (scanner.hasNextLine()) {
-				tmp = scanner.nextLine().split(":");
-
-				proxies.put(tmp[0], Integer.valueOf(tmp[1]));
+			List<String> stringList = Files.readAllLines(Path.of(PROXY_LIST_FILE_NAME));
+			for (String s : stringList) {
+				csv.append(s);
 			}
-			scanner.close();
+			GeonodeProxyList geonodeProxyList = new Gson().fromJson(csv.toString(), GeonodeProxyList.class);
+
+			for (GeonodeProxyList.Data data : geonodeProxyList.data) {
+			CompletableFuture.runAsync(() -> {
+					try {
+						Jsoup.connect(LYRICS_PROVIDER_SEARCH_URI)
+								.proxy(data.ip, Integer.parseInt(data.port))
+								.userAgent(USER_AGENT)
+								.timeout(TIMEOUT_MS)
+								.get();
+
+						ACTIVE_PROXIES.add(data);
+						LOGGER.info("Proxy - {}:{} - Added", data.ip, data.port);
+
+					} catch (IOException e) {
+						LOGGER.warn("Proxy - {}:{} - {}", data.ip, data.port, e.getLocalizedMessage());
+					}
+			});
+		}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		LOGGER.info("Done parsing proxy list in memory!");
 	}
 
 	public Lyrics() {
@@ -134,6 +195,11 @@ public class Lyrics extends Command {
 	}
 
 	private void jSoupScrape(String search, TextChannel textChannel) {
+		if (ACTIVE_PROXIES.size() == 0) {
+			textChannel.sendMessage("No servers available for routing. If I just restart, please wait a few minutes and try again.").submit();
+			return;
+		}
+
 		String         lyricsURL;
 		String         title;
 		String         artist;
@@ -148,16 +214,20 @@ public class Lyrics extends Command {
 		int            nMessages;
 
 		/* Build URI */
-		query = "https://search.azlyrics.com/search.php?q=" + search;
-		String proxy = (String) Arrays.asList(proxies.keySet().toArray()).get((int) (Math.random() * proxies.size()));
+		query = LYRICS_PROVIDER_SEARCH_URI + search;
+
+		int proxyChoice = (int) (Math.random() * ACTIVE_PROXIES.size());
+
+		String ip   = ACTIVE_PROXIES.get(proxyChoice).ip;
+		String port = ACTIVE_PROXIES.get(proxyChoice).port;
 
 		/* Connect to and retrieve a DOM from host provider */
 		try {
-			Connection.Response response = Jsoup.connect(query)
-					.proxy(proxy, proxies.get(proxy))
-					.userAgent("Mozilla")
-					.referrer("http://www.google.com")
-					.followRedirects(true)
+			Connection.Response response = Jsoup
+					.connect(query)
+					.proxy(ip, Integer.parseInt(port))
+					.userAgent(USER_AGENT)
+					.timeout(TIMEOUT_MS)
 					.execute();
 
 			if (response.statusCode() == 403) {
@@ -165,11 +235,14 @@ public class Lyrics extends Command {
 				return;
 			}
 
-
-
 			doc = response.parse();
 		} catch (IOException e) {
-			LOGGER.warn("IOException - " + e.getCause().getMessage());
+			if (e instanceof ConnectException) {
+				LOGGER.warn("Connect Exception - {}", e.getCause().getMessage());
+			} else {
+				e.printStackTrace();
+			}
+			jSoupScrape(search, textChannel);
 			return;
 		}
 
@@ -185,10 +258,9 @@ public class Lyrics extends Command {
 		lyricsURL = searchResultElement.attr("href");
 		try {
 			Connection.Response response = Jsoup.connect(lyricsURL)
-					.proxy(proxy, proxies.get(proxy))
-					.userAgent("Mozilla")
-					.referrer("http://www.google.com")
-					.followRedirects(true)
+					.proxy(ip, Integer.parseInt(port))
+					.userAgent(USER_AGENT)
+					.timeout(0)
 					.execute();
 
 			if (response.statusCode() == 403) {
@@ -198,7 +270,7 @@ public class Lyrics extends Command {
 
 			doc = response.parse();
 		} catch (IOException e) {
-			LOGGER.warn("IOException - " + e.getCause().getMessage());
+			e.printStackTrace();
 			return;
 		}
 
